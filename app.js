29853a236f74179a7aef893e0a67d9df151a0253
F Bliss <60014136+voffee@users.noreply.github.com>
2023-01-27 18:04:07 +0800

    Built In Constructors - Complete

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 
-// Person constructor
+// String
 
-function Person(name, dob) {
-    this.name = name;
-    // this.age = age;
-    this.birthday = new Date(dob);
-    this.calculateAge = function() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+const name1 = 'Jeff'; //primitive so the type will be string and value Jeff
+
+const name2 = new String('Jeff'); // Type will be object with VALUE of Jeff
+
+// name2.foo = 'bar'; // Property of foo and value of 'bar'
+// console.log(name2);
+
+console.log(typeof name2);
+
+if (name2 == 'Jeff') {
+    // Double == will validate the value but not type
+    // Triple === will validate value and type
+    console.log('YES');
+} else {
+    console.log('NO');
 }
 
-// const brad = new Person('Brad', 36);
-// const miles = new Person('Miles', 30);
+// Number
+const num1 = 5;
+const num2 = new Number(5);
 
-// console.log(miles.age);
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
 
-const brad = new Person('Brad', '9-10-1981');
+// Functions
+const getSum1 = function(x, y) {
+return x + y;
+}
 
-console.log(brad.calculateAge());
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+// Object
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+
+console.log(john2);
+
+// Arrays
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
+
+// Can use this way of creating arrays, it is not useful for the other types of structures.
+console.log(arr1);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re2);
