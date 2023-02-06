9206f401a6723b511da4c384258549e4044b1b43
F Bliss <60014136+voffee@users.noreply.github.com>
2023-02-06 14:41:43 +0800

    Subclasses completed.

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 
 class Person {
-    constructor(firstName, lastName, dob) {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.birthday = new Date(dob);
     }
 
     greeting() {
         return `Hello there ${this.firstName} ${this.lastName}`;
     }
-    calculateAge() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
+
+        this.phone = phone;
+        this.membership = membership;
     }
-    getsMarried(newLastName) {
-        this.lastName = newLastName;
-    }
-    static addNumbers(x, y) {
-        return x + y;
+
+    static getMembershipCost() {
+        return 500;
     }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
 
-mary.getsMarried('Thompson');
-console.log(mary);
+console.log(john.greeting());
 
-console.log(Person.addNumbers(1,2));
+console.log(Customer.getMembershipCost());
+
+// When calling static methods, call by parent class.
