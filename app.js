e2bfb2782097845f4d707ae6b441d7116f4307a6
F Bliss <60014136+voffee@users.noreply.github.com>
2023-02-06 10:20:33 +0800

    Classes complete

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 
-const personProtoypes = {
-    greeting: function() {
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dob);
+    }
+
+    greeting() {
         return `Hello there ${this.firstName} ${this.lastName}`;
-    },
-    getsMarried: function(newLastName) {
+    }
+    calculateAge() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+    getsMarried(newLastName) {
         this.lastName = newLastName;
+    }
+    static addNumbers(x, y) {
+        return x + y;
     }
 }
 
-const mary = Object.create(personProtoypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
+console.log(mary);
 
-console.log(mary.greeting());
-
-const brad = Object.create(personProtoypes, {
-    firstName: {value: 'Brad'},
-    lastName: {value: 'Traversy'},
-    age: {age: 36}
-});
-
-console.log(brad);
-console.log(brad.greeting());
+console.log(Person.addNumbers(1,2));
